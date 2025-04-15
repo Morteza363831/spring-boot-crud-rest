0f5de148fea975b69c0ccf6fd491ed0786f04b48
morteza363831 <morteza363831official@gmail.com>
2025-04-15 23:39:32 +0330

    @Data replaced with @Setter and @Getter

--- src/main/java/org/example/springbootcrudrest/model/CustomerUpdateDto.java
+++ src/main/java/org/example/springbootcrudrest/model/CustomerUpdateDto.java
@@ -3,17 +3,16 @@ package org.example.springbootcrudrest.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
 /**
- * DTO for {@link Customer}
+ * DTO for {@link Customer} -> mutable
  */
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
