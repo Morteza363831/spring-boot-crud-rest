2821fabd8cbf093466d35ebc1879e52d9168f381
morteza363831 <morteza363831official@gmail.com>
2025-04-15 21:13:00 +0330

    project initialized

---
+++ src/main/java/org/example/springbootcrudrest/SpringBootCrudRestApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootcrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudRestApplication.class, args);
+    }
+
+}
