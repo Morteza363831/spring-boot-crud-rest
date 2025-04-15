de68d8eaea330c539ddf790c798955a934ce3b81
morteza363831 <morteza363831official@gmail.com>
2025-04-16 00:54:11 +0330

    separate Database persistence and in memory persistence logic for CustomerService

---
+++ src/main/java/org/example/springbootcrudrest/service/CustomerPersistenceService.java
@@ -0,0 +1,9 @@
+package org.example.springbootcrudrest.service;
+
+import org.example.springbootcrudrest.model.Customer;
+
+public interface CustomerPersistenceService {
+
+    Customer createCustomer(Customer customer);
+    void updateCustomer(String name, Customer customer);
+}
