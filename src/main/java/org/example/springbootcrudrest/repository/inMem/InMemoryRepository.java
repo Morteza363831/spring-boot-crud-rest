428b586e428acb920661060ad0eca76dd2a929c4
morteza363831 <morteza363831official@gmail.com>
2025-04-15 21:47:39 +0330

    InMemoryRepository and InMemoryRepositoryImpl added

---
+++ src/main/java/org/example/springbootcrudrest/repository/inMem/InMemoryRepository.java
@@ -0,0 +1,23 @@
+package org.example.springbootcrudrest.repository.inMem;
+
+import org.example.springbootcrudrest.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InMemoryRepository {
+
+    Optional<Customer> findById(Long id);
+
+    Optional<Customer> findByUsername(String username);
+
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    void delete(Customer customer);
+
+    void update(Customer customer);
+
+    void refresh();
+}
---
+++ src/main/java/org/example/springbootcrudrest/repository/inMem/InMemoryRepositoryImpl.java
@@ -0,0 +1,83 @@
+package org.example.springbootcrudrest.repository.inMem;
+
+import lombok.NonNull;
+import org.example.springbootcrudrest.model.Customer;
+import org.example.springbootcrudrest.repository.db.CustomerRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+
+public class InMemoryRepositoryImpl implements InMemoryRepository {
+
+    // properties
+    private final List<Customer> customers;
+
+    // injection
+    private final CustomerRepository customerRepository;
+
+    public InMemoryRepositoryImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+        this.customers = customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(@NonNull Long id) {
+        return customers
+                .stream()
+                .filter(customer -> customer.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<Customer> findByUsername(@NonNull String username) {
+        return customers
+                .stream()
+                .filter(customer -> customer.getUsername().equalsIgnoreCase(username))
+                .findFirst();
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers);
+    }
+
+    @Override
+    public void save(@NonNull Customer customer) {
+        customers.stream()
+                .filter(c -> c.getUsername().equalsIgnoreCase(customer.getUsername()))
+                .findFirst()
+                .ifPresentOrElse(c -> {}, () -> customers.add(customer));
+    }
+
+    @Override
+    public void delete(@NonNull Customer customer) {
+        customers.removeIf(c -> c.getUsername().equalsIgnoreCase(customer.getUsername()));
+    }
+
+    @Override
+    public void update(@NonNull Customer customer) {
+        customers.stream()
+                .filter(c -> c.getUsername().equalsIgnoreCase(customer.getUsername()))
+                .findFirst()
+                .ifPresent(c -> {
+                    partialUpdate(c, customer);
+                });
+    }
+
+    @Override
+    public void refresh() {
+        customers.clear();
+        customers.addAll(customerRepository.findAll());
+    }
+
+    private void partialUpdate(Customer oldCustomer, Customer newCustomer) {
+        oldCustomer.setUsername(newCustomer.getUsername());
+        oldCustomer.setPassword(newCustomer.getPassword());
+        oldCustomer.setFirstname(newCustomer.getFirstname());
+        oldCustomer.setLastname(newCustomer.getLastname());
+    }
+}
