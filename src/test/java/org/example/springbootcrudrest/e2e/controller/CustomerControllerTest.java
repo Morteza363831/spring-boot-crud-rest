5761cfe2a62cdd42603f51910c93efb64585813b
morteza363831 <morteza363831official@gmail.com>
2025-04-18 22:47:21 +0330

    controller moved to e2e package , repo moved to integration package

--- src/test/java/org/example/springbootcrudrest/e2e/controller/CustomerControllerTest.java
+++ src/test/java/org/example/springbootcrudrest/e2e/controller/CustomerControllerTest.java
@@ -1,4 +1,4 @@
-package org.example.springbootcrudrest.unit.controller;
+package org.example.springbootcrudrest.e2e.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
--- src/test/java/org/example/springbootcrudrest/integration/repo/DatabaseRepositoryTest.java
+++ src/test/java/org/example/springbootcrudrest/integration/repo/DatabaseRepositoryTest.java
@@ -1,8 +1,11 @@
-package org.example.springbootcrudrest.unit.repo;
+package org.example.springbootcrudrest.integration.repo;
 
 import org.example.springbootcrudrest.model.Customer;
 import org.example.springbootcrudrest.repository.db.CustomerRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
